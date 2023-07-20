8557f2f14132c37bae771dcc96175fb4a3ec2f0e
Martin Eigenmann <github@eigenmannmartin.ch>
2023-07-20 16:37:28 +0200

    feat: update mappings

--- nvchad/mappings.lua
+++ nvchad/mappings.lua
@@ -5,13 +5,13 @@ M.general = {
   n = {
     -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-h>"] = { "<cmd>TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd>TmuxNavigateRight<CR>", "window left" },
-    ["<C-j>"] = { "<cmd>TmuxNavigateDown<CR>", "window left" },
-    ["<C-k>"] = { "<cmd>TmuxNavigateUp<CR>", "window left" },
+    ["<C-l>"] = { "<cmd>TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd>TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd>TmuxNavigateUp<CR>", "window up" },
     ["<CR>"] = { "<cmd>FineCmdline<CR>", "enter command mode", opts = { noremap = true } },
-    ["<leader>R"] = { "<cmd>Neotest run<CR>", "run tests", opts = { noremap = true } },
-    ["<leader>RS"] = { "<cmd>Neotest summary<CR>", "toggle neotest summary", opts = { noremap = true } },
-    ["<leader>RA"] = {
+    ["<leader>rr"] = { "<cmd>Neotest run<CR>", "run tests", opts = { noremap = true } },
+    ["<leader>rs"] = { "<cmd>Neotest summary<CR>", "toggle neotest summary", opts = { noremap = true } },
+    ["<leader>ra"] = {
       function ()
         require("neotest").run.run(vim.fn.expand("%"))
       end
