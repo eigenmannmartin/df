ac15a3ec4820baca61fb5c44b17707b9b02cd54c
Martin Eigenmann <github@eigenmannmartin.ch>
2024-10-07 07:38:53 +0200

    mount x11

--- toolkit/dcup.sh
+++ toolkit/dcup.sh
@@ -7,6 +7,6 @@ devcontainer up \
   --mount type=bind,source="$PWD"/.devcontainer/,target=/mnt/devcontainer \
   --mount type=bind,source="$HOME"/.1password/agent.sock,target=/home/vscode/.1password/agent.sock \
   --mount type=bind,source=/opt/1Password/op-ssh-sign,target=/opt/1Password/op-ssh-sign \
-  --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix,consistency=cached \
+  --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
   --dotfiles-repository $DOTFILES_REPOSITORY \
   --additional-features='{"ghcr.io/eigenmannmartin/devcontainer-features/neovim:0": {"version":"nightly"}, "ghcr.io/eigenmannmartin/devcontainer-features/xdg-open:0": {}, "ghcr.io/eigenmannmartin/devcontainer-features/neovim-essentials:0": {}, "ghcr.io/devcontainers/features/sshd:1": {}, "ghcr.io/devcontainers/features/java:1.6.0": {"version":"latest"}}' $@
